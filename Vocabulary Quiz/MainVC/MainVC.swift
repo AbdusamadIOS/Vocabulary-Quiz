ce2051a1ba4763bab7f944ce9f9caa9f520060aa
javlonbekdev <javlonbekdev@gmail.com>
2024-04-03 15:42:24 +0500

    bug fixed

--- Vocabulary Quiz/MainVC/MainVC.swift
+++ Vocabulary Quiz/MainVC/MainVC.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    
     @IBOutlet weak var nounLbl: UILabel!
     @IBOutlet weak var nextBtn: UIButton!
     @IBOutlet weak var conteneirView: UIView!
@@ -25,24 +25,23 @@ class MainVC: UIViewController {
         super.viewDidLoad()
         
         setupTCollectionView()
-        configureUi(question: Datas.list.first)
+        configureUi(question: Datas.list.filter{ $0.type == .test }.randomElement()!)
         setupNavBar()
         progressTime()
         conteneirView.layer.cornerRadius = 15
         conteneirView.clipsToBounds = true
         nextBtn.layer.cornerRadius = 7
         timePV.layer.cornerRadius = 4
-    
     }
     func setupNavBar() {
         navigationItem.title = "Question"
         navigationItem.hidesBackButton = true
         
         let navigationBarAppearance = UINavigationBarAppearance()
-            navigationBarAppearance.titleTextAttributes = [
-                NSAttributedString.Key.foregroundColor: UIColor.white,
-                NSAttributedString.Key.font: UIFont.systemFont(ofSize: 25, weight: .bold) ]
-            navigationController?.navigationBar.standardAppearance = navigationBarAppearance
+        navigationBarAppearance.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 25, weight: .bold) ]
+        navigationController?.navigationBar.standardAppearance = navigationBarAppearance
         
         let back = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .done, target: self, action: #selector(backBtn))
         back.tintColor = .white
@@ -63,56 +62,25 @@ class MainVC: UIViewController {
         
     }
     // TODO: Check Answer function
-    private func checkAnswer(answer: String, question: QuestionModel) -> Bool {
-        return Datas.list.answers.contains(where: { $0.text == answer.text }) && answer.correct
-        
-        }
+    
     // TODO: Configure Ui Update
     private func configureUi(question: QuestionModel) {
         questionLbl.text = question.question
         nounLbl.text = ""
         currentQuestion = question
         collectionView.reloadData()
-    
-    }
-    // TODO: Next Button
-    @IBAction func nextBtn(_ sender: UIButton) {
-        timePV.progress = timePV.progress - 1
-        if timePV.progress == 0 {
-            print("tugadi va keyingi savol")
-           let question = currentQuestion
-//            _ = question?.answers
-            if let index = Datas.list.firstIndex(where: { $0.answer0 == question?.answer1}) {
-                if index < (UnitB1.vocubularies.count - 1) {
-                    let nextQuestion = Datas.list[index + 1]
-                    currentQuestion = nil
-                    configureUi(question: nextQuestion)
-                    timePV.progress = 1
-                } else {
-                    timer?.invalidate()
-                    let scoreVC = ScoreVC(nibName: "ScoreVC", bundle: nil)
-                    scoreVC.result = sum
-                    self.navigationController?.setViewControllers([scoreVC], animated: true)
-                }
-            }
-        }
-    }
-    
-    // TODO: ProgressView time and setting
-    func progressTime() {
-       timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timeBtn), userInfo: nil, repeats: true)
         
     }
-    // Time Button
-    @objc func timeBtn() {
-//        timePV.progress = timePV.progress - 0.12
+    // TODO: Next Button
+//    @IBAction func nextBtn(_ sender: UIButton) {
+//        timePV.progress = timePV.progress - 1
 //        if timePV.progress == 0 {
 //            print("tugadi va keyingi savol")
-//           let question = currentQuestion
-//            _ = question?.answers
-//            if let index = UnitB1.vocubularies.firstIndex(where: { $0.text == question?.text}) {
+//            let question = currentQuestion
+//            //            _ = question?.answers
+//            if let index = Datas.list.firstIndex(where: { $0.answer0 == question?.answer1}) {
 //                if index < (UnitB1.vocubularies.count - 1) {
-//                    let nextQuestion = UnitB1.vocubularies[index + 1]
+//                    let nextQuestion = Datas.list[index + 1]
 //                    currentQuestion = nil
 //                    configureUi(question: nextQuestion)
 //                    timePV.progress = 1
@@ -124,6 +92,34 @@ class MainVC: UIViewController {
 //                }
 //            }
 //        }
+//    }
+    
+    // TODO: ProgressView time and setting
+    func progressTime() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timeBtn), userInfo: nil, repeats: true)
+        
+    }
+    // Time Button
+    @objc func timeBtn() {
+        //        timePV.progress = timePV.progress - 0.12
+        //        if timePV.progress == 0 {
+        //            print("tugadi va keyingi savol")
+        //           let question = currentQuestion
+        //            _ = question?.answers
+        //            if let index = UnitB1.vocubularies.firstIndex(where: { $0.text == question?.text}) {
+        //                if index < (UnitB1.vocubularies.count - 1) {
+        //                    let nextQuestion = UnitB1.vocubularies[index + 1]
+        //                    currentQuestion = nil
+        //                    configureUi(question: nextQuestion)
+        //                    timePV.progress = 1
+        //                } else {
+        //                    timer?.invalidate()
+        //                    let scoreVC = ScoreVC(nibName: "ScoreVC", bundle: nil)
+        //                    scoreVC.result = sum
+        //                    self.navigationController?.setViewControllers([scoreVC], animated: true)
+        //                }
+        //            }
+        //        }
     }
 }
 // MARK: Extension CollectionViewDataSourche and CollectionViewDelegate
@@ -148,34 +144,29 @@ extension MainVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollec
         
         let answer = Datas.list.first?.answers?[indexPath.row]
         
-        if checkAnswer(answer: answer, question: question) {
+        if Datas.list[indexPath.row].answer == answer {
             // correct answer
-            if let index = UnitB1.vocubularies.firstIndex(where: { $0.text == question.text}) {
-                if index < (UnitB1.vocubularies.count - 1) {
-                    sum += 1
-                    let nextQuestion = UnitB1.vocubularies[index + 1]
-                    currentQuestion = nil
-                    configureUi(question: nextQuestion)
-                    timePV.progress = 1
-                   
-                } else {
-                    timer?.invalidate()
-                    let scoreVC = ScoreVC(nibName: "ScoreVC", bundle: nil)
-                    scoreVC.result = sum
-                    self.navigationController?.setViewControllers([scoreVC], animated: true)
-                }
-            }
+            //            if let index = UnitB1.vocubularies.firstIndex(where: { $0.text == question.text}) {
+            //                if index < (UnitB1.vocubularies.count - 1) {
+            sum += 1
+            let nextQuestion = Datas.list[sum]
+            currentQuestion = nil
+            configureUi(question: nextQuestion)
+            timePV.progress = 1
+            
+            //                } else {
+            //                    timer?.invalidate()
+            //                    let scoreVC = ScoreVC(nibName: "ScoreVC", bundle: nil)
+            //                    scoreVC.result = sum
+            //                    self.navigationController?.setViewControllers([scoreVC], animated: true)
+            //                }
+            //            }
         } else {
             // Wrong answer
-            if let index = UnitB1.vocubularies.firstIndex(where: { $0.text == question.text}) {
-                if index < (UnitB1.vocubularies.count - 1) {
-                    let nextQuestion = UnitB1.vocubularies[index + 1]
-                    currentQuestion = nil
-                    configureUi(question: nextQuestion)
-                    timePV.progress = 1
-                    
-                }
-            }
+            let nextQuestion = Datas.list[sum]
+            currentQuestion = nil
+            configureUi(question: nextQuestion)
+            timePV.progress = 1
         }
     }
 }
